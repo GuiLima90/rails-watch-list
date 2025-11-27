c45f065cfc5954dd0741752afcd14f28ac996517
Guilherme Starnino <starninoguilherme@gmail.com>
2025-11-27 20:40:13 +0100

    Lista

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+require "json"
+require "open-uri"
+
+puts "Limpando o banco..."
+Movie.destroy_all
+
+puts "Buscando filmes da API..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Criando filmes..."
+movies["results"].each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Filmes criados com sucesso!"
+
+
 #
 # Example:
 #
